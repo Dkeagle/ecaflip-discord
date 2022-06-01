49de26c58cf85560e8869f663d098bec9700663b
Dkeagle <taildeman_dylan@outlook.com>
2022-06-01 12:02:05 +0200

    corrected a few typos

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import os
-from dotenv import load_dotenv
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
 
 # Importing bot modules
 from modules.config import PREFIX, NAME
